5b96199ecf39dc269e2c7bff814a616c97ba6a00
ivan <ivansamusev044@gmail.com>
2023-01-05 17:58:28 +0200

    car & toy

---
+++ index.js
@@ -0,0 +1,12 @@
+ let car = 13000
+ let toy = 120
+
+console.log('car = ' , car)
+console.log('toy = ' , toy)
+
+const changes = 'changes!'
+car = 25000
+toy = 170
+console.log(changes)
+console.log('car = ' , car)
+console.log('toy = ' , toy)
